a1f0a057c6e7f292aaaf905bc3a34a8593a5c81c
Haron <haron.kamau@yahoo.com>
2025-07-05 15:24:53 +0930

    Add files via upload

---
+++ mountain-goat-vite-starter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to The Mountain Goat Farm 🐐</h1>
+      <p>This is a Vite + React starter site.</p>
+    </div>
+  )
+}
+
+export default App
